2f794eef02b384b3793c37a96163fc92ea2c80b7
remysedlak <remysedlak@gmail.com>
2025-07-12 22:05:18 -0400

    udpate clipboard

--- src/clipboard.rs
+++ src/clipboard.rs
@@ -1,25 +1,35 @@
 use copypasta::{ClipboardContext, ClipboardProvider};
-use std::{thread, time::Duration};
+use std::{
+    error::Error,
+    sync::mpsc::{self, Receiver, Sender},
+    thread::{self, JoinHandle},
+    time::Duration,
+};
 use chrono::Utc;
 
-pub fn monitor_clipboard<F>(on_change: F)
+pub fn monitor_clipboard<F>(
+    on_change: F,
+) -> Result<(Sender<()>, JoinHandle<()>), Box<dyn Error>>
 where
     F: Fn(String, String) + Send + 'static,
 {
-    thread::spawn(move || {
-        let mut ctx = ClipboardContext::new().unwrap();
+    let (stop_tx, stop_rx): (Sender<()>, Receiver<()>) = mpsc::channel();
+    let mut ctx = ClipboardContext::new()?;
+    let handle = thread::spawn(move || {
         let mut last_clip = String::new();
-
         loop {
+            if stop_rx.try_recv().is_ok() {
+                break;
+            }
             if let Ok(current) = ctx.get_contents() {
-                if current != last_clip && !current.trim().is_empty() {
+                if !current.trim().is_empty() && current != last_clip {
                     let timestamp = Utc::now().to_rfc3339();
-                    on_change(current.clone(), timestamp.clone());
+                    on_change(current.clone(), timestamp);
                     last_clip = current;
                 }
             }
-
             thread::sleep(Duration::from_millis(500));
         }
     });
+    Ok((stop_tx, handle))
 }
